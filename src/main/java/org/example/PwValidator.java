d2fa2ad121c81136d64656c9919725eddaeece88
stefansiebers <s3f3s-github@yahoo.com>
2024-08-21 16:07:18 +0200

    test and implement isLongEnough(), hasUpperAndLowerCase() and hasNumber()

--- src/main/java/org/example/PwValidator.java
+++ src/main/java/org/example/PwValidator.java
@@ -2,15 +2,29 @@ package org.example;
 
 public class PwValidator {
     public static boolean isLongEnough(String pw){
-        return false;
+        return pw.length()>=8;
     }
 
     public static boolean hasNumber(String pw){
+        for (char c='0'; c<='9'; c++){
+            if (pw.indexOf(c)!=-1) return true;
+        };
         return false;
     }
 
     public static boolean hasUpperAndLowercase(String pw){
-        return false;
+        boolean foundUpper = false;
+        boolean foundLower = false;
+        for (int i=0; i<pw.length(); i++) {
+            char c = pw.charAt(i);
+            if (Character.isUpperCase(c)) {
+                foundUpper = true;
+            }
+            if (Character.isLowerCase(c)) {
+                foundLower = true;
+            }
+        }
+        return foundUpper && foundLower;
     }
 
     public static boolean isNoCommonPassword(String pw){
--- src/test/java/org/example/PwValidatorTest.java
+++ src/test/java/org/example/PwValidatorTest.java
@@ -1,7 +1,46 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PwValidatorTest {
+
+    @Test
+    void isLongEnough_shouldReturnFalseWhenCalledWith1234567(){
+        String testPw = "1234567";
+        assertFalse(PwValidator.isLongEnough(testPw));
+    }
+
+    @Test
+    void isLongEnough_shouldReturnTrueWhenCalledWith12345678(){
+        String testPw = "12345678";
+        assertTrue(PwValidator.isLongEnough(testPw));
+    }
+
+    @Test
+    void hasNumber_shouldReturnFalseWhenCalledWithLettersOnly(){
+        assertFalse(PwValidator.hasNumber("Dieser Text enthält keine Ziffern"));
+    }
+
+    @Test
+    void hasNumber_shouldReturnTrueWhenCalledWithDigitInPw(){
+        assertTrue(PwValidator.hasNumber("Dieser Text enthält 1 Ziffer"));
+    }
+
+    @Test
+    void hasUpperAndLowercase_shouldReturnFalseWhenCalledWithLowercaseOnly(){
+        assertFalse(PwValidator.hasUpperAndLowercase("alles kleingeschrieben"));
+    }
+
+    @Test
+    void hasUpperAndLowercase_shouldReturnFalseWhenCalledWithUppercaseOnly(){
+        assertFalse(PwValidator.hasUpperAndLowercase("HIER IST ALLES GROSS"));
+    }
+
+    @Test
+    void hasUpperAndLowercase_shouldReturnTrueWhenCalledWithMixedcase(){
+        assertTrue(PwValidator.hasUpperAndLowercase("Fast alles kleingeschrieben"));
+    }
 
 }
