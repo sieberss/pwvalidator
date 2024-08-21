c2a13592a913443f31b4ef0c96ae9350bb9469ee
stefansiebers <s3f3s-github@yahoo.com>
2024-08-21 15:04:21 +0200

    move methods to new Class and create test class

---
+++ src/main/java/org/example/PwValidator.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class PwValidator {
+    public static boolean isLongEnough(String pw){
+        return false;
+    }
+
+    public static boolean hasNumber(String pw){
+        return false;
+    }
+
+    public static boolean hasUpperAndLowercase(String pw){
+        return false;
+    }
+
+    public static boolean isNoCommonPassword(String pw){
+        return false;
+    }
+
+}
